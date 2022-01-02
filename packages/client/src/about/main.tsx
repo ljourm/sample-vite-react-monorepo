a04f56ff307e8ab58cd10b92f4339aaa56daf3e8
ljourm <ljourm@gmail.com>
2022-01-03 05:14:58 +0900

    feat: マルチhtml構成に変更

---
+++ packages/client/src/about/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <p>about</p>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const rootDir = "src";
+
+const htmlFilePaths = ["index.html", "about/index.html"].map((path) =>
+  resolve(__dirname, rootDir, path)
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  root: "src",
+  plugins: [react()],
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: htmlFilePaths,
+    },
+  },
+});
