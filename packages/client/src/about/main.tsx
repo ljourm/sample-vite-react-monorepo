ede24e05fe253cf622cb013e0269e7a2dcf16c0e
ljourm <ljourm@gmail.com>
2022-01-03 05:33:34 +0900

    feat: 環境変数のサンプルの追加

--- packages/client/src/about/main.tsx
+++ packages/client/src/about/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const appTitle = import.meta.env.VITE_APP_TITLE;
+const sampleNumber = import.meta.env.VITE_SAMPLE_NUMBER;
+
 ReactDOM.render(
   <React.StrictMode>
-    <p>about</p>
+    <p>app title: {appTitle}</p>
+    <p>sample number: {sampleNumber}</p>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- packages/client/src/vite-env.d.ts
+++ packages/client/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_SAMPLE_NUMBER: undefined;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
