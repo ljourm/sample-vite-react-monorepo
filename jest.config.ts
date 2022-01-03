1f4a49681e6db22b24fff0bdbf7f463e4b687d7b
ljourm <ljourm@gmail.com>
2022-01-04 02:11:05 +0900

    feat: coverage計測の導入

--- jest.config.ts
+++ jest.config.ts
@@ -4,4 +4,14 @@ module.exports = {
   transform: {
     "^.+\\.(t|j)sx?$": "@swc/jest",
   },
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  collectCoverage: true,
+  coverageReporters: ["html", "text-summary"],
+  collectCoverageFrom: [
+    "packages/client/**/*",
+    "!**/node_modules/**",
+    "!**/.storybook/**",
+    "!**/*.stories.*",
+    "!packages/client/src/pages/about/app.tsx", // TODO: Viteの環境変数を読み込めないため除外。要検討。
+  ],
 };
