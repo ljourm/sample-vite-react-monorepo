ccdb04d288493eaceadcf56c57370edb31115e67
ljourm <ljourm@gmail.com>
2022-01-04 03:00:34 +0900

    feat: viteのページ一覧の管理方法の変更

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,9 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import pageList from "./pagelist";
 
 const rootDir = "src/pages";
-
-const htmlFilePaths = ["index.html", "about/index.html"].map((path) => resolve(__dirname, rootDir, path));
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,7 +17,7 @@ export default defineConfig({
     outDir: resolve(__dirname, "dist"),
     emptyOutDir: false,
     rollupOptions: {
-      input: htmlFilePaths,
+      input: pageList.map((path) => resolve(__dirname, rootDir, path)),
     },
   },
   plugins: [react()],
