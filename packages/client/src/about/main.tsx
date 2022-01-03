89ca630cb8d06653fc6e4202e3d593caf461a854
ljourm <ljourm@gmail.com>
2022-01-03 17:39:52 +0900

    feat: sassの導入

--- packages/client/src/about/main.tsx
+++ packages/client/src/about/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "~/styles/app.scss";
+
 const appTitle = import.meta.env.VITE_APP_TITLE;
 const apiBaseUri = import.meta.env.VITE_API_BASE_URI;
 
